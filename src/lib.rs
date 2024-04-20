8ffab35484976b77bd79535dba47ef0fc2282440
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2024-04-20 18:04:15 +0200

    Fix in test for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -618,23 +618,19 @@ mod tests {
     #[test]
     #[ignore = "Longer version of previous test"]
     fn test_encrypted_data_extensive() {
-        const N: usize = 128;
+        const N: usize = 256;
         let params = Parameters::new(128, 128, 64, 128);
         let mut context = Context::new(params);
 
         let mut rng = rand::thread_rng();
-        for i in 0..N {
+        for _ in 0..N {
             context.generate_secret_key(&mut rng);
             context.generate_public_key(&mut rng);
 
             let data = Data::from_usize(rng.gen());
             let encrypted_data = data.encrypt(context.get_public_key().unwrap(), &mut rng);
             let decrypted_data = encrypted_data.decrypt(context.get_secret_key().unwrap());
-            if data.to_usize() != decrypted_data.to_usize() {
-                println!("O : {} -- D : {}", data.to_usize(), decrypted_data.to_usize());
-            } else {
-                println!("OK {}", i);
-            }
+            assert_eq!(data.to_usize(), decrypted_data.to_usize());
         }
     }
 }
